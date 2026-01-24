acd9891699d422d5a14f24252d2410dc2494d606
Sakib Mohammed Sobaha <1905021@ugrad.cse.buet.ac.bd>
2026-01-24 17:29:08 +0600

    gitignore added

--- python_basics/dict.py
+++ python_basics/dict.py
@@ -22,3 +22,23 @@ for value in d1.values():
 
 for key in d1.keys():
     print(key)
+
+# 6 - Dict
+alien_0 = {'color': 'green', 'points': 5}
+print(alien_0['color'])
+
+print(f"The alien is {alien_0['color']}")
+alien_0['color'] = 'yellow'
+print(f"The alien is now {alien_0['color']}")
+
+alien_0 = {'x_position': 0, 'y_position': 25, 'speed': 'medium'}
+
+if alien_0['speed'] == 'slow':
+    x_increment = 1
+elif alien_0['speed'] == 'medium':
+    x_increment = 2
+else:
+    x_increment = 3
+
+alien_0['x_position'] = alien_0['x_position'] + x_increment
+print(alien_0['x_position'])
